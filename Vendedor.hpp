87d55850404b02752eb513ef0c57389d632b0341
gabrielhw <73146914+gabrielhw@users.noreply.github.com>
2020-10-20 20:34:38 -0300

    Update Vendedor.hpp

--- Vendedor.hpp
+++ Vendedor.hpp
@@ -1,1 +1,13 @@
+#ifndef #VENDEDOR_H
+#define #VENDEDOR_H
+#include "Empregado.hpp"
 
+class Vendedor : public Empregado {
+
+  public:
+    std::string nome;  	  
+	
+  double quotaTotalAnual();
+	
+};
+#endif
