5e0a777277cadaa5c781aa7428b7ed9a7abda7e3
gabrielhw <73146914+gabrielhw@users.noreply.github.com>
2020-10-20 21:41:23 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,6 +1,12 @@
 #include <string>
 #include "Vendedor.hpp"
 
+Vendedor::Vendedor(std::string nome, double quotaMensalVendas, double _salarioHora) {
+  _nome = nome;
+  _quotaMensalVendas = quotaMensalVendas;
+  salarioHora = _salarioHora;
+}
+
 double Vendedor::quotaTotalAnual() {
   return quotaMensalVendas * 12;
 }
