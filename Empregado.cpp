76885c0489b690f6f945dfb0c865e5f22a749a45
gabrielhw <73146914+gabrielhw@users.noreply.github.com>
2020-10-20 21:12:06 -0300

    Update Empregado.cpp

--- Empregado.cpp
+++ Empregado.cpp
@@ -1,10 +1,11 @@
 #include "Empregado.hpp"
+#define DIARIA 8 //jornada diaria em horas
 
 double Empregado::pagamentoMes(double horasTrabalhadas) {
   double t = horasTrabalhadas;
 	//Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-  if (horasTrabalhadas > 8) {
-    double x = horasTrabalhadas - 8;
+  if (horasTrabalhadas > DIARIA) {
+    double x = horasTrabalhadas - DIARIA;
     t += x / 2;
   }
 	return t * salarioHora;
