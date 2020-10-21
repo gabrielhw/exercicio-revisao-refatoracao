f9236c28896377effaf8848505a9045d81f39118
gabrielhw <73146914+gabrielhw@users.noreply.github.com>
2020-10-20 22:31:05 -0300

    Update Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -4,7 +4,7 @@
 Engenheiro::Engenheiro(std::string nome, int projetos, double _salarioHora){
   _nome = nome;
   _projetos = projetos;
-  salarioHora = _salarioHora
+  salarioHora = _salarioHora;
 };
 int Engenheiro::getProjetos(){
   return _projetos; 
