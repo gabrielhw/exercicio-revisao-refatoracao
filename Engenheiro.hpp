1d42e8d29bcd14e0429708b60759a257f72b621d
gabrielhw <73146914+gabrielhw@users.noreply.github.com>
2020-10-20 22:37:59 -0300

    Update Engenheiro.hpp

--- Engenheiro.hpp
+++ Engenheiro.hpp
@@ -6,11 +6,9 @@ class Engenheiro : public Empregado {
 
   public:
     Engenheiro(std::string nome, int projetos, double _salarioHora);
-    std::string getNome();
     int getProjetos();
   private:
-    std::string _nome;  
     int _projetos;
-
+  
 };
 #endif
