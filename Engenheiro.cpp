80bf037c79492bfaff874c516bd1bc7d8af0ad3e
gabrielhw <73146914+gabrielhw@users.noreply.github.com>
2020-10-20 21:17:22 -0300

    Update Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,2 +1,15 @@
 #include <string>
 #include "Engenheiro.hpp"
+
+Engenheiro::Engenheiro(std:string nome, int projetos, double _salarioHora){
+  _nome = nome;
+  _projetos = projetos;
+  salarioHora = _salarioHora
+}
+std::string Engenheiro::getNome(){
+  return _nome; 
+}
+int Engenheiro::getProjetos(){
+  return _projetos; 
+}
+
